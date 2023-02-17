fd045621c07caa726d09cd826684aac4f5eb0c98
josemanuelsoltero <josemanuelsoltero@joses-MacBook-Pro.local>
2023-02-17 16:14:30 -0600

    added post functionality

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,8 @@ const PORT = 3001;
 const api = require('./routes/notes')
 const fs = require('fs')
 
-const notes = [];
+
+const noteStuff = require('./routes/notes')
 
 
 app.use(express.json());
@@ -22,52 +23,8 @@ app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.use(noteStuff);
 
-
-app.post('/api/notes', (req, res) =>{
-    console.log(`${req.method} request received to add a note`);
-
-    if (product && review && username) {
-        // Variable for the object we will save
-        const newReview = {
-          product,
-          review,
-          username,
-          upvotes: Math.floor(Math.random() * 100),
-          review_id: uuid(),
-        };
-    
-        // Convert the data to a string so we can save it
-        const reviewString = JSON.stringify(newReview);
-    
-        // Write the string to a file
-        fs.writeFile(`./db/${newReview.product}.json`, reviewString, (err) =>
-          err
-            ? console.error(err)
-            : console.log(
-                `Review for ${newReview.product} has been written to JSON file`
-              )
-        );
-    
-        const response = {
-          status: 'success',
-          body: newReview,
-        };
-    
-        console.log(response);
-        res.status(201).json(response);
-      } else {
-        res.status(500).json('Error in posting review');
-      }
-    })
-
-
-
-
-
-
-
-
-
-app.listen(PORT, () => console.log('listening on port 3001'));
-
+app.listen(PORT, () => {
+  console.log('Listening......')
+})
